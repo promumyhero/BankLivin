ab752f773fd908bcb9ec478f20a72e3651c4c67d
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-06-30 16:47:32 +0700

    Update on Total Balances

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Hery' };
   return (
-    <div>Home</div>
+    <section className="home">
+    <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Akses dan manage akun Mandiri Anda dengan mudah."
+            />
+
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+            />
+
+
+        </header>
+    </div>
+    </section>
   )
 }
 
