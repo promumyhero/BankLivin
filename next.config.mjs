4095fb9bef8337a247a39335bbd34df2fff7ab82
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-07-09 15:35:26 +0700

    add new modify

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript {
-        ignoreBuildErrors: true,
+        ignoreBuildErrors: true
     },
     eslint: {
-        ignoreDuringBuilds: true,
+        ignoreDuringBuilds: true
     }
 };
 
