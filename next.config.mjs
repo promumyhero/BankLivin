c7d2c8d616caa3d6df1cb90ec874fc5ab3357f28
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-07-09 15:41:50 +0700

    modify

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript {
+    typescript: {
         ignoreBuildErrors: true
     },
     eslint: {
